3f71ea43f2d368ae42695b7d0058933e75731768
waleed-max <36965138+waleed-max@users.noreply.github.com>
2020-04-26 18:12:50 -0500

    Add APP file

---
+++ E-commerce 2/src/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import './App.css';
+
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component'
+import {auth, createUserProfileDocument} from './firebase/firebase.utils'
+
+class App extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      currentUser : null,
+      name: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
+      this.setState({currentUser: user})
+      
+      this.setState({name: user.displayName})
+      console.log(user.displayName)
+      // createUserProfileDocument(user);
+      
+    });
+    
+
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render(){;
+  return (
+    <div>
+      <Header currentUser={this.state.currentUser} name={this.state.name}/>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
+      </Switch>
+    </div>
+  );
+}}
+
+
+export default App;
